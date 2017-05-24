069a5adda7b6ba43910e20e93e5cf650c0a692ae
localkingheping <756684837@qq.com>
2017-05-24 20:51:16 +0800

    11

--- app/src/main/java/hp/hp/com/heping05243/MainActivity.java
+++ app/src/main/java/hp/hp/com/heping05243/MainActivity.java
@@ -3,6 +3,9 @@ package hp.hp.com.heping05243;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 2132334234
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
